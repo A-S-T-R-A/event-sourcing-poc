da369ec503229eb4d59398a95ed8df0b9cf1df83
jesuitdelray <jesuitdelray@gmail.com>
2024-04-06 15:20:20 +0300

    calendar version

--- server/eventsource.js
+++ server/eventsource.js
@@ -21,14 +21,27 @@ app.get("/connect", (req, res) => {
     }
     emitter.on("newMessage", onMessage)
 
+    const onEvent = event => {
+        res.write(`event: newEvent\ndata: ${JSON.stringify(event)} \n\n`)
+    }
+    emitter.on("newEvent", onEvent)
+
     res.on("close", () => {
         emitter.removeListener("newMessage", onMessage)
+        emitter.removeListener("newEvent", onEvent)
     })
 })
 
 app.post("/new-messages", (req, res) => {
     const message = { ...req.body, id: Date.now() }
     emitter.emit("newMessage", message)
+    res.status(204).end()
+})
+
+app.post("/add-event", (req, res) => {
+    const { title, start, end } = req.body
+    const event = { title, start, end, id: Date.now() }
+    emitter.emit("newEvent", event)
     res.status(204).end()
 })
 
